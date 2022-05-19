22b96148d930e8bbf8ce91ce9992de1fca400e16
jyunmitch <j.yunmitchell@entando.com>
2022-05-19 12:05:37 -0500

    gitignore file and 173 files changed

---
+++ ui/widgets/conference/formWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,17 @@
+const conferenceMockEdit = {
+  id: -892,
+  name:
+    'Culpa rerum et debitis ut ad adipisci et. In dicta explicabo assumenda. Doloribus ipsum sed dolor. Nam eius quia debitis in autem sed. Temporibus et dignissimos aliquam et quia soluta delectus vel.',
+  location:
+    'Reprehenderit sed dolore voluptate enim illum suscipit. Dolores corrupti minima nobis commodi repudiandae incidunt cumque. Velit hic corporis esse dicta minus velit velit. Error natus deserunt velit sit dolorem ad sed. Numquam corrupti voluptates est sint sapiente est. Voluptas enim asperiores animi veritatis reiciendis.',
+};
+
+const conferenceMockAdd = {
+  id: '',
+  name:
+    'Culpa rerum et debitis ut ad adipisci et. In dicta explicabo assumenda. Doloribus ipsum sed dolor. Nam eius quia debitis in autem sed. Temporibus et dignissimos aliquam et quia soluta delectus vel.',
+  location:
+    'Reprehenderit sed dolore voluptate enim illum suscipit. Dolores corrupti minima nobis commodi repudiandae incidunt cumque. Velit hic corporis esse dicta minus velit velit. Error natus deserunt velit sit dolorem ad sed. Numquam corrupti voluptates est sint sapiente est. Voluptas enim asperiores animi veritatis reiciendis.',
+};
+
+export { conferenceMockAdd, conferenceMockEdit };
---
+++ ui/widgets/conference/tableWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,18 @@
+const conferenceMocks = [
+  {
+    id: -892,
+    name:
+      'Culpa rerum et debitis ut ad adipisci et. In dicta explicabo assumenda. Doloribus ipsum sed dolor. Nam eius quia debitis in autem sed. Temporibus et dignissimos aliquam et quia soluta delectus vel.',
+    location:
+      'Reprehenderit sed dolore voluptate enim illum suscipit. Dolores corrupti minima nobis commodi repudiandae incidunt cumque. Velit hic corporis esse dicta minus velit velit. Error natus deserunt velit sit dolorem ad sed. Numquam corrupti voluptates est sint sapiente est. Voluptas enim asperiores animi veritatis reiciendis.',
+  },
+  {
+    id: 952,
+    name:
+      'Ea dolor aspernatur ut ut laborum facilis numquam. Corporis a aut accusamus quisquam. Natus non magni dignissimos. Repellendus itaque id doloremque sit.',
+    location:
+      'Ut fugiat maiores sunt quis itaque modi. Ad voluptas magnam maiores molestiae unde illum est sint. Facere quos enim rerum nam excepturi voluptatem. Ullam qui non ipsam molestias. Et nihil qui quam eum id tempora ea.',
+  },
+];
+
+export default conferenceMocks;
